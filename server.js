e3df8c1488c6616398db9f35fa4394c73da43a5e
trickpattyFH20 <patricktlawler@gmail.com>
2016-05-13 05:06:45 +0000

    stable bundled server U WOT M8?!?!

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var express = require('express');
 var app = express();
-app.use('/', express.static(__dirname + '/')); // ← adjust
+var path = require('path');
+//app.use('/', express.static(__dirname + '/')); // ← adjust
+app.use(express.static(path.join(__dirname, './')));
 app.listen(8080, function() { console.log('listening'); });
