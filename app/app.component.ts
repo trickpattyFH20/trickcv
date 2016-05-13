83d64b11cce6c0b96f78ef7cea7f5a7114310b7f
trickpattyFH20 <patricktlawler@gmail.com>
2016-05-12 23:14:25 -0400

    bundle angular / fix cloudflare email protection

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,4 +5,8 @@ import { Component } from '@angular/core';
   templateUrl: 'app/app.component.html'
 })
 
-export class AppComponent { }
+export class AppComponent {
+    public sendEmail(){
+        window.location.href = "mailto:patricktlawler@gmail.com";
+    }
+}
