2720c6ff7be6150b4044ccaeca7d5d3887ed1ebc
trickpattyFH20 <patricktlawler@gmail.com>
2016-05-13 02:11:19 -0400

    refactor to express

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-//app.use('/', express.static(__dirname + '/')); // ← adjust
-app.use(express.static(path.join(__dirname, './')));
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
+app.use('/', express.static(__dirname + '/public'));
 app.listen(8080, function() { console.log('listening'); });
