0d35306e3f730b6cfbfb03ed4d88454d08d4d906
trickpattyFH20 <patricktlawler@gmail.com>
2016-05-13 00:18:29 -0400

    temp server fix

--- server.js
+++ server.js
@@ -1,62 +1,31 @@
-var http = require('http');
-var fs = require('fs');
-var path = require('path');
-var index = fs.readFileSync('index.html');
+var http = require('http')
+var url = require('url')
+var fs = require('fs')
+var path = require('path')
+var baseDirectory = __dirname   // or whatever base directory you want
+
+var port = 8080
 
 http.createServer(function (request, response) {
-    console.log('request starting...');
+   try {
+     var requestUrl = url.parse(request.url)
 
-    var filePath = '.' + request.url;
-    if (filePath == './')
-        filePath = './index.html';
+     // need to use path.normalize so people can't access directories underneath baseDirectory
+     if(requestUrl.pathname == '/' || requestUrl.pathname == ''){
+         requestUrl.pathname = '/index.html';
+     }
+     var fsPath = baseDirectory+path.normalize(requestUrl.pathname)
 
-    var extname = path.extname(filePath);
-    var contentType = 'text/html';
-    switch (extname) {
-        case '.js':
-            contentType = 'text/javascript';
-            break;
-        case '.css':
-            contentType = 'text/css';
-            break;
-        case '.json':
-            contentType = 'application/json';
-            break;
-        case '.png':
-            contentType = 'image/png';
-            break;
-    }
-
-    if(filePath.indexOf('?v=') != -1){
-        console.log('matches regex')
-        if(filePath.indexOf('.css') != -1){
-            contentType = 'text/css';
-        }
-        if(filePath.indexOf('.js') != -1){
-            contentType = 'text/javascript';
-        }
-        var end = filePath.indexOf("?");
-        filePath = filePath.substring(0, end);
-    }
-
-    fs.readFile(filePath, function(error, content) {
-        if (error) {
-            if(error.code == 'ENOENT'){
-                fs.readFile('./404.html', function(error, content) {
-                    response.writeHead(200, { 'Content-Type': contentType });
-                    response.end(content, 'utf-8');
-                });
-            }
-            else {
-                response.writeHead(500);
-                response.end('Server error: '+error.code+' ..\n');
-                response.end();
-            }
-        }
-        else {
-            response.writeHead(200, { 'Content-Type': contentType });
-            response.end(content, 'utf-8');
-        }
-    });
-
-}).listen(8080);
+     response.writeHead(200)
+     var fileStream = fs.createReadStream(fsPath)
+     fileStream.pipe(response)
+     fileStream.on('error',function(e) {
+         response.writeHead(404)     // assume the file doesn't exist
+         response.end()
+     })
+   } catch(e) {
+     response.writeHead(500)
+     response.end()     // end the response so browsers don't hang
+     console.log(e.stack)
+   }
+}).listen(port)
