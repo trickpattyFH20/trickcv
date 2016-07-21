ad88da461d059a8f549e3390ef4881da7e8f349d
trickpattyFH20 <patricktlawler@gmail.com>
2016-07-20 23:58:31 -0400

    add https and keybase proof

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express = require('express');
+var https = require('https');
+var http = require('http');
 var app = express();
 var path = require('path');
+var fs = require('fs');
+
 app.use('/node_modules', express.static(__dirname + '/node_modules'));
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -10,4 +14,15 @@ app.use('/forscience', forscience);
 app.use('/', express.static(__dirname + '/public'));
 app.use('/*', express.static(__dirname + '/public'));
 
-app.listen(8080, function() { console.log('listening'); });
+//app.listen(8080, function() { console.log('listening'); });
+
+var pkey = fs.readFileSync('key.pem');
+var pcert = fs.readFileSync('cert.pem')
+
+var options = {
+    key: pkey,
+    cert: pcert
+};
+
+http.createServer(app).listen(8080);
+https.createServer(options, app).listen(443);
