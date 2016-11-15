9dddde648ae61b31c50c605a90a627a8911cb8c0
trickpattyFH20 <patricktlawler@gmail.com>
2016-11-15 00:11:32 -0500

    decouple twilio and es6 convert

--- server.js
+++ server.js
@@ -1,62 +1,27 @@
-var express = require('express');
-var https = require('https');
-var http = require('http');
-var app = express();
-var path = require('path');
-var fs = require('fs');
-var twilio = require('twilio');
-var express = require('express');
-var bodyParser = require('body-parser');
-
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.post('/voice', function (req, res) {
-    console.log(req.body);
-    // Create TwiML response
-    var twiml = new twilio.TwimlResponse();
-
-    if(req.body.To) {
-        twiml.dial({ }, function() {
-            // wrap the phone number or client name in the appropriate TwiML verb
-            // by checking if the number given has only digits and format symbols
-            if (/^[\d\+\-\(\) ]+$/.test(req.body.To)) {
-                console.log('1st condition');
-
-                var numberDialer = function(dial) {
-                    dial.number(req.body.To);
-                };
-
-                twiml.dial({callerId: '+18475011344' }, numberDialer);
-
-            } else {
-
-                console.log('2nd condition')
-                this.client(req.body.To);
-            }
-        });
-    } else {
-        twiml.say("Thanks for calling!");
-    }
-
-    res.set('Content-Type', 'text/xml');
-    res.send(twiml.toString());
-});
+let express = require('express'),
+    https = require('https'),
+    http = require('http'),
+    app = express(),
+    path = require('path'),
+    fs = require('fs')
 
 app.use('/node_modules', express.static(__dirname + '/node_modules'));
 app.use(express.static(path.join(__dirname, 'public/app')));
 
-var forscience = require('./csapi');
+let forscience = require('./csapi'),
+    twilioVoice = require('./twilio');
 
 app.use('/forscience', forscience);
+app.use('/voice', twilioVoice);
 app.use('/', express.static(__dirname + '/public/app/prod'));
 app.use('/*', express.static(__dirname + '/public/app/prod'));
 
 //app.listen(8080, function() { console.log('listening'); });
 
-var pkey = fs.readFileSync('key.pem');
-var pcert = fs.readFileSync('cert.pem')
+let pkey = fs.readFileSync('key.pem'),
+    pcert = fs.readFileSync('cert.pem')
 
-var options = {
+let options = {
     key: pkey,
     cert: pcert
 };
---
+++ twilio/index.js
@@ -0,0 +1,37 @@
+let twilio = require('twilio'),
+    bodyParser = require('body-parser'),
+    router = require('express').Router();
+
+router.use(bodyParser.urlencoded({ extended: false }));
+
+router.post('/voice', (req, res) => {
+    // Create TwiML response
+    let twiml = new twilio.TwimlResponse();
+
+    if(req.body.To) {
+        twiml.dial({ }, () => {
+            // wrap the phone number or client name in the appropriate TwiML verb
+            // by checking if the number given has only digits and format symbols
+            if (/^[\d\+\-\(\) ]+$/.test(req.body.To)) {
+
+                let numberDialer = dial => {
+                    dial.number(req.body.To);
+                };
+
+                twiml.dial({callerId: '+18475011344' }, numberDialer);
+
+            } else {
+
+                console.log('2nd condition')
+                this.client(req.body.To);
+            }
+        });
+    } else {
+        twiml.say("Thanks for calling!");
+    }
+
+    res.set('Content-Type', 'text/xml');
+    res.send(twiml.toString());
+});
+
+module.exports = router;
