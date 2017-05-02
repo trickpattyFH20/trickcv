73e40e144ada0cb67a5b69d247916923aaba3ead
Patrick <patricktlawler@gmail.com>
2017-05-02 01:04:38 -0400

    Update about.component.ts

--- src/client/app/about/about.component.ts
+++ src/client/app/about/about.component.ts
@@ -16,7 +16,6 @@ declare var YT: any;
 
 export class AboutComponent implements OnInit {
 
-
     muted: boolean = true;
     isMobile: boolean = false;
     player: any;
@@ -43,77 +42,24 @@ export class AboutComponent implements OnInit {
 
     onPlayerStateChange(event: any) {
         console.log('this: ', this);
-    //   if (event.data === YT.PlayerState.PLAYING && !this.done) {
-    //     this.done = true;
-    //   }
     }
-
-    // public onYouTubeIframeAPIReady = function onYouTubeIframeAPIReady() {
-    //   player = new YT.Player('player', {
-    //     height: '390',
-    //     width: '640',
-    //     videoId: 'M7lc1UVf-VE',
-    //     events: {
-    //       'onReady': onPlayerReady,
-    //       'onStateChange': onPlayerStateChange
-    //     }
-    //   });
-    // }
 
     onPlayerReady(event: any) {
+        const isMobile = {
+            Android: function() { return navigator.userAgent.match(/Android/i); },
+            BlackBerry: function() { return navigator.userAgent.match(/BlackBerry/i); },
+            iOS: function() { return navigator.userAgent.match(/iPhone|iPad|iPod/i); },
+            Opera: function() { return navigator.userAgent.match(/Opera Mini/i); },
+            Windows: function() { return navigator.userAgent.match(/IEMobile/i); },
+            any: function() { return (isMobile.Android() ||
+                isMobile.BlackBerry() ||
+                isMobile.iOS() ||
+                isMobile.Opera() ||
+                isMobile.Windows());
+            }
+        };
 
-      var isMobile = {
-          Android: function() { return navigator.userAgent.match(/Android/i); },
-          BlackBerry: function() { return navigator.userAgent.match(/BlackBerry/i); },
-          iOS: function() { return navigator.userAgent.match(/iPhone|iPad|iPod/i); },
-          Opera: function() { return navigator.userAgent.match(/Opera Mini/i); },
-          Windows: function() { return navigator.userAgent.match(/IEMobile/i); },
-          any: function() { return (isMobile.Android() ||
-               isMobile.BlackBerry() ||
-               isMobile.iOS() ||
-               isMobile.Opera() ||
-               isMobile.Windows());
-          }
-      };
-
-    //   if(isMobile.any() !== null) {
-    //       this.isMobile = true;
-    //       this.player.cuePlaylist({'list':'FLzULymNZwMkZqaaNIFcp3fQ'});
-    //   } else {
-    //       this.player.loadPlaylist({'list':'FLzULymNZwMkZqaaNIFcp3fQ'});
-    //   }
-
-    this.player.loadPlaylist({'list':'FLzULymNZwMkZqaaNIFcp3fQ'});
-
-      this.player.mute();
-    }
-
-    ngOnInit() {
-    //   var tag = document.createElement('script');
-      //
-    //   tag.src = 'https://www.youtube.com/iframe_api';
-    //   var firstScriptTag = document.getElementsByTagName('script')[0];
-    //   firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-      //
-    //   // 3. This function creates an <iframe> (and YouTube player)
-    //   //    after the API code downloads.
-      //
-    //   this.player = new YT.Player('playertwo', {
-    //     height: 'auto',
-    //     width: '300',
-    //     playerVars: {
-    //         'controls': 1
-    //      },
-    //     events: {
-    //       'onReady': this.onPlayerReady,
-    //       'onStateChange': this.onPlayerStateChange
-    //     }
-    //   });
-      //
-    //   // 5. The API calls this function when the player's state changes.
-    //   //    The function indicates that when playing a video (state=1),
-    //   //    the player should play for six seconds and then stop.
-    //   this.done = false;
-
+        this.player.loadPlaylist({'list':'FLzULymNZwMkZqaaNIFcp3fQ'});
+        this.player.mute();
     }
 }
